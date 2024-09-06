3faaa417c8bf9382b95d0d1ad71fdf5f56094ef9
민지원 <mgw7730@envalue.biz>
2024-09-06 13:10:08 +0900

    1

--- src/test/java/com/coco/board/controller/UserControllerTest.java
+++ src/test/java/com/coco/board/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.coco.board.controller;
 
 import com.coco.board.application.dto.UserDto;
 import com.coco.board.domain.Role;
+import com.coco.board.domain.User;
+import com.coco.board.infrastructure.persistence.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,23 +13,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.MediaType;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @SpringBootTest
-//@Transactional
-//@AutoConfigureMockMvc
+@Transactional
 @AutoConfigureMockMvc(addFilters = false)  // Security 필터 비활성화
 public class UserControllerTest {
 
@@ -38,36 +43,47 @@ public class UserControllerTest {
     private ClientRegistrationRepository clientRegistrationRepository;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     ObjectMapper objectMapper;
 
-//    @Autowired
-//    WebApplicationContext webApplicationContext;
-
-//    @BeforeEach
-//    public void setup() {
-//        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();  // MockMvc 객체 초기화
-//    }
-
     @Test
-    @DisplayName("정상 회원가입 통합 테스트")
+    @DisplayName("Join Successfully")
     public void join_shouldSuccess() throws Exception {
         // given
         UserDto.Request userRequest = UserDto.Request.builder()
-                .id(93L)
+                .id(12L)
                 .role(Role.USER)
                 .email("test@te.st")
-                .password("password")
-                .nickname("testNickName")
+                .password("Password123123!")
+                .nickname("test")
                 .username("test123")
                 .build();
 
-        String aa = objectMapper.writeValueAsString(userRequest);
         // when
-        mockMvc.perform(MockMvcRequestBuilders.post("/auth/joinProc")
+        MvcResult response = mockMvc.perform(MockMvcRequestBuilders.post("/auth/joinProc")
                 .with(csrf())
-                .content(objectMapper.writeValueAsString(userRequest))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .param("id", String.valueOf(userRequest.getId()))
+                .param("role", String.valueOf(userRequest.getRole()))
+                .param("email", userRequest.getEmail())
+                .param("password", userRequest.getPassword())
+                .param("nickname", userRequest.getNickname())
+                .param("username", userRequest.getUsername())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andReturn();
+
         // then
+        Optional<User> foundUser_opt =  userRepository.findByUsername(userRequest.getUsername());
+        assertTrue(foundUser_opt.isPresent());
+
+        User foundUser = foundUser_opt.get();
+        User userRequestEntity = userRequest.toEntity();
+
+        assertEquals(302, response.getResponse().getStatus());
+        assertEquals(foundUser.getEmail(), userRequestEntity.getEmail());
+        assertEquals(foundUser.getUsername(), userRequestEntity.getUsername());
+        assertEquals(foundUser.getRole(), userRequestEntity.getRole());
+        assertEquals(foundUser.getNickname(), userRequestEntity.getNickname());
     }
 }
